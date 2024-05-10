fc53e024df4a466b84b04b95acd8ac9ccbd07b26
syoung01 <165664062+syoung01@users.noreply.github.com>
2024-05-11 00:39:32 +0900

    Create MapFragment.java (코드 추가 전)

---
+++ RDMD/ui/map/MapFragment.java
@@ -0,0 +1,17 @@
+package androidtown.org.termproject.ui.map;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+import androidtown.org.termproject.ui.dashboard.DashboardViewModel;
+
+public class MapFragment extends Fragment {
+
+}
