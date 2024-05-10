3bd8225e6fd9fa20ea59ca53c5f86ee19fb608b2
syoung01 <165664062+syoung01@users.noreply.github.com>
2024-05-11 00:41:10 +0900

    Create RankFragment.java (코드 추가 전)

---
+++ RDMD/ui/rank/RankFragment.java
@@ -0,0 +1,14 @@
+package androidtown.org.termproject.ui.rank;
+
+import android.os.Bundle;
+import android.view.LayoutInflater;
+import android.view.View;
+import android.view.ViewGroup;
+import android.widget.TextView;
+
+import androidx.annotation.NonNull;
+import androidx.fragment.app.Fragment;
+import androidx.lifecycle.ViewModelProvider;
+
+public class RankFragment extends Fragment {
+}
